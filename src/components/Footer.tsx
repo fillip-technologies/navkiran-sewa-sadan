ea9071df2e4df2e6d32b2f42023a1231cc529be4
Prince80848 <princeraj808488@gmail.com>
2026-06-02 17:46:39 +0530

    addres change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -32,7 +32,7 @@ const Footer = () => {
           getInTouch: "Get In Touch",
           supportText:
             "Reach out to us for support or to learn more about our services.",
-          addressLines: ["Navkiran seva sadan, anand bihar colony opposite to bharat petroleum boothnath patna -26, Bihar "],
+          addressLines: ["Anand bihar colony opposite to bharat petroleum boothnath bypass or near jeevan deep hospital patna -800026"],
           copyright: "Navkiran Seva Sadan. All rights reserved.",
           madeWith: "Made with",
           madeForHumanity: "for humanity.",
@@ -53,7 +53,7 @@ const Footer = () => {
           getInTouch: "संपर्क में रहें",
           supportText:
             "मदद पाने या हमारी सेवाओं के बारे में जानने के लिए हमसे संपर्क करें।",
-          addressLines: ["नवकिरण सेवा सदन, आनंद बिहार कॉलोनी, भारत पेट्रोलियम बूथनाथ के सामने, पटना-26, बिहार"],
+          addressLines: ["आनंद बिहार कॉलोनी, भारत पेट्रोलियम के सामने, भूतनाथ बाइपास या जीवन दीप अस्पताल के पास, पटना - 800026"],
           copyright: "नवकिरण सेवा सदन। सभी अधिकार सुरक्षित।",
           madeWith: "दिल से",
           madeForHumanity: "मानवता की सेवा के लिए।",
